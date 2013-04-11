f804641a3ecac023af6dd3b66e24d119f3042a08
shade88 <shade88@yandex.ru>
2013-04-11 23:26:14 +0600

    changed blog message controller tests

--- spec/controllers/blog_messages_controller_spec.rb
+++ spec/controllers/blog_messages_controller_spec.rb
@@ -1,17 +1,34 @@
 require 'spec_helper'
 
 describe BlogMessagesController do
-  before(:each) do
-    sign_in User.find(1)
-  end
-  it "creates a new message" do
-    BlogMessage.should_receive(:new).with("text" => "a quick brown fox","user_id"=>"1")
-    post :create, :blog_message => { "text" => "a quick brown fox",user_id:1}
-  end
+  #before(:each) do
+  #  #sign_in FactoryGirl.create(:user)
+  #end
+  #it "creates a new message" do
+  #  BlogMessage.should_receive(:new).with("text" => "a quick brown fox","user_id"=>"1")
+  #  post :create, :blog_message => { "text" => "a quick brown fox",user_id:1}
+  #end
+  #
+  #it "redirects to the Messages index" do
+  #  post :create
+  #  response.should redirect_to(blog_messages_path)
+  #end
 
-  it "redirects to the Messages index" do
-    post :create
-    response.should redirect_to(:action => "index")
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new contact" do
+        expect{
+          post :create, blog_message: FactoryGirl.attributes_for(:blog_message)
+        }.to change(BlogMessage,:count).by(1)
+      end
+      it "saves the new contact in the database"
+      it "redirects to the home page"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new contact in the database"
+      it "re-renders the :new template"
+    end
   end
 
 end
